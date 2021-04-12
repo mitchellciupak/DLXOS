7277c665fd80b0d451a6e913631c7624afc801aa
mitchellciupak <ciupak@purdue.edu>
2021-04-11 20:25:15 -0400

    part 3 test created, but not tested

--- one-level/apps/example/p3/p3.c
+++ one-level/apps/example/p3/p3.c
@@ -1,26 +1,31 @@
+#include "os/memory_constants.h"
 #include "usertraps.h"
 #include "misc.h"
 
 void main (int argc, char *argv[])
 {
   sem_t s_procs_completed; // Semaphore to signal the original process that we're done
+  int * location: //location in memory of process
 
-  if (argc != 2) { 
-    Printf("Usage: %s <handle_to_procs_completed_semaphore>\n"); 
+  if (argc != 2) {
+    Printf("Usage: %s <handle_to_procs_completed_semaphore>\n");
     Exit();
-  } 
+  }
 
   // Convert the command-line strings into integers for use as handles
   s_procs_completed = dstrtol(argv[1], NULL, 10);
 
   // Now print a message to show that everything worked
-  Printf("hello_world (%d): Hello world!\n", getpid());
+  Printf("Part 2 (%d): Out of Max Virtual Address being Attempted!\n", getpid());
 
   // Signal the semaphore to tell the original process that we're done
   if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
-    Printf("hello_world (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
+    Printf("Part 2 (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
     Exit();
   }
 
-  Printf("hello_world (%d): Done!\n", getpid());
+  location = (MEM_MAX_VIRTUAL_ADDRESS << 2);
+  Printf("Part 2 (%d): Acessing Location: (%d)!\n", getpid(), location);
+  Printf("Part 2 (%d): (%d)!\n", getpid(), *location);
+  Printf("Part 2 (%d): Done!\n", getpid());
 }
